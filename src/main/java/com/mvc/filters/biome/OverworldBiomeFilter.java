ce387b98f1ae686558ed4720999027eba5568c4b
PacManMVC <maxim.vancompernolle@gmail.com>
2025-11-13 20:35:01 -0500

    reuse and optimize biome layers hopefully do some runtime analysis jungle and mega taiga bugged

--- src/main/java/com/mvc/filters/biome/OverworldBiomeFilter.java
+++ src/main/java/com/mvc/filters/biome/OverworldBiomeFilter.java
@@ -24,6 +24,7 @@ public class OverworldBiomeFilter {
     private final IntBiomeLayer biomeLayer16;
     private final IntBiomeLayer biomeLayer19;
     private final IntBiomeLayer biomeLayer26;
+    private final IntBiomeLayer biomeLayer31;
     private final ArrayList<CPos> mushroomPositions;
     private final ArrayList<CPos> badlandsPositions;
     private final ArrayList<CPos> junglePositions;
@@ -39,6 +40,7 @@ public class OverworldBiomeFilter {
         this.biomeLayer16 = overworldBiomeSource.getLayer(16);
         this.biomeLayer19 = overworldBiomeSource.getLayer(19);
         this.biomeLayer26 = overworldBiomeSource.getLayer(26);
+        this.biomeLayer31 = overworldBiomeSource.getLayer(31);
         this.mushroomPositions = new ArrayList<>();
         this.badlandsPositions = new ArrayList<>();
         this.junglePositions = new ArrayList<>();
@@ -51,7 +53,14 @@ public class OverworldBiomeFilter {
     }
 
     public boolean filterBiomes() {
-        return hasBadlandsBiomes();
+        /*
+        mushroom 2m 23s 30 seeds 10,276 matches
+        jungle 1m 37s 18 seeds 10,129 matches ~ BUGGED SOMETIMES???
+        mega taiga 38s 8 seeds 10,909 ~ BUGGED SOMETIMES???
+        badlands ~ SLOW AS FUCK
+        snowy 53s 10 seeds 11,547 matches
+         */
+        return hasBiomeTiles() && hasSnowyBiomes();
     }
 
     private boolean hasVillage() {
@@ -123,32 +132,7 @@ public class OverworldBiomeFilter {
         return false;
     }
 
-    private boolean hasBiomes() {
-        /*
-        TODO: add checks for individual biomes
-        mushroom_fields - layer 16 256:1
-        mushroom_field_shore - layer 31 16:1
-
-        jungle - layer 18 256:1
-        jungle_hills - layer 26 64:1
-        jungle_edge - layer 31 16:1
-        bamboo_jungle - layer 19 256:1
-        bamboo_jungle_hills - layer 26 64:1
-
-        badlands - layer 22 64:1
-        badlands_plateau - layer 18 256:1
-        wooded_badlands_plateau - layer 18 256:1
-
-        giant_tree_taiga - layer 18 256:1
-        giant_tree_taiga_hills - layer 26 64:1
-
-        snowy_tundra - layer 18 256:1
-        snowy_mountains - layer 26 64:1
-        snowy_taiga - layer 18 256:1
-        snowy_taiga_hills - layer 26 64:1
-        frozen_river - layer 41 4:1
-        */
-
+    private boolean hasBiomeTiles() {
         ArrayList<CPos> specialPositions = new ArrayList<>();
         long specialLayerSeed = BiomeLayer.getLayerSeed(worldSeed, 3);
 
@@ -169,7 +153,6 @@ public class OverworldBiomeFilter {
             return false;
         }
 
-        ArrayList<CPos> mushroomPositions = new ArrayList<>();
         long mushroomLayerSeed = BiomeLayer.getLayerSeed(worldSeed, 5);
 
         // 76% to have 1 mushroom tile
@@ -222,40 +205,39 @@ public class OverworldBiomeFilter {
             for (int z = -3; z <= 2; z++) {
                 if (biomeLayer11.sample(x, 0, z) == 4) {
                     freezing = true;
+                    snowyPositions.add(new CPos(x, z));
                 }
             }
         }
 
-        if (!freezing) {
-            return false;
-        }
+        return freezing;
+    }
 
+    private boolean hasMushroomBiomes() {
+        /*
+        id 14 is mushroom_fields
+        checking at 256:1
+
+        id 15 is mushroom_field_shore
+        checking at 16:1
+        */
         for (CPos pos: mushroomPositions) {
             if (biomeLayer16.sample(pos.getX(), 0, pos.getZ()) == 14) {
-                return true;
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        int x_16 = pos.getX() * 16 + x;
+                        int z_16 = pos.getZ() * 16 + z;
+                        if (biomeLayer31.sample(x_16, 0, z_16) == 15) {
+                            return true;
+                        }
+                    }
+                }
             }
         }
-
         return false;
     }
 
     private boolean hasJungleBiomes() {
-        long specialLayerSeed = BiomeLayer.getLayerSeed(worldSeed, 3);
-        long specialLocalSeed = BiomeLayer.getLocalSeed(specialLayerSeed, 0, 0);
-        if (Math.floorMod(specialLocalSeed >> 24, 13) != 0) {
-            return false;
-        }
-
-        // id 0 is ocean
-        if (biomeLayer9.sample(0, 0, 0) == 0) {
-            return false;
-        }
-
-        // id 2 is desert -> jungle
-        if (biomeLayer11.sample(0, 0, 0) != 2) {
-            return false;
-        }
-
         /*
         id 168 is bamboo_jungle
         checking at 256:1
@@ -263,13 +245,19 @@ public class OverworldBiomeFilter {
         id 169 is bamboo_jungle_hills
         checking at 64:1
         */
-        for (int x = 0; x < 4; x++) {
-            for (int z = 0; z < 4; z++) {
-                if (biomeLayer19.sample(x, 0, z) == 168) {
-                    for (int i = 0; i < 4; i++) {
-                        for (int j = 0; j < 4; j++) {
-                            if (biomeLayer26.sample(i, 0, j) == 169) {
-                                return true;
+        for (CPos pos: junglePositions) {
+            for (int x = 0; x < 4; x++) {
+                for (int z = 0; z < 4; z++) {
+                    int x_256 = pos.getX() * 4 + x;
+                    int z_256 = pos.getZ() * 4 + z;
+                    if (biomeLayer19.sample(x_256, 0, z_256) == 168) {
+                        for (int i = 0; i < 4; i++) {
+                            for (int j = 0; j < 4; j++) {
+                                int x_64 = x_256 * 4 + i;
+                                int z_64 = z_256 * 4 + j;
+                                if (biomeLayer26.sample(x_64, 0, z_64) == 169) {
+                                    return true;
+                                }
                             }
                         }
                     }
@@ -280,22 +268,6 @@ public class OverworldBiomeFilter {
     }
 
     private boolean hasMegaTaigaBiomes() {
-        long specialLayerSeed = BiomeLayer.getLayerSeed(worldSeed, 3);
-        long specialLocalSeed = BiomeLayer.getLocalSeed(specialLayerSeed, 0, 0);
-        if (Math.floorMod(specialLocalSeed >> 24, 13) != 0) {
-            return false;
-        }
-
-        // id 0 is ocean
-        if (biomeLayer9.sample(0, 0, 0) == 0) {
-            return false;
-        }
-
-        // id 3 is mountains -> mega taiga
-        if (biomeLayer11.sample(0, 0, 0) != 3) {
-            return false;
-        }
-
         /*
         id 32 is giant_tree_taiga
         checking at 256:1
@@ -303,13 +275,19 @@ public class OverworldBiomeFilter {
         id 33 is giant_tree_taiga_hills
         checking at 64:1
         */
-        for (int x = 0; x < 4; x++) {
-            for (int z = 0; z < 4; z++) {
-                if (biomeLayer19.sample(x, 0, z) == 32) {
-                    for (int i = 0; i < 4; i++) {
-                        for (int j = 0; j < 4; j++) {
-                            if (biomeLayer26.sample(i, 0, j) == 33) {
-                                return true;
+        for (CPos pos: megaTaigaPositions) {
+            for (int x = 0; x < 4; x++) {
+                for (int z = 0; z < 4; z++) {
+                    int x_256 = pos.getX() * 4 + x;
+                    int z_256 = pos.getZ() * 4 + z;
+                    if (biomeLayer19.sample(x_256, 0, z_256) == 32) {
+                        for (int i = 0; i < 4; i++) {
+                            for (int j = 0; j < 4; j++) {
+                                int x_64 = x_256 * 4 + i;
+                                int z_64 = z_256 * 4 + j;
+                                if (biomeLayer26.sample(x_64, 0, z_64) == 33) {
+                                    return true;
+                                }
                             }
                         }
                     }
@@ -320,22 +298,6 @@ public class OverworldBiomeFilter {
     }
 
     private boolean hasBadlandsBiomes() {
-        long specialLayerSeed = BiomeLayer.getLayerSeed(worldSeed, 3);
-        long specialLocalSeed = BiomeLayer.getLocalSeed(specialLayerSeed, 0, 0);
-        if (Math.floorMod(specialLocalSeed >> 24, 13) != 0) {
-            return false;
-        }
-
-        // id 0 is ocean
-        if (biomeLayer9.sample(0, 0, 0) == 0) {
-            return false;
-        }
-
-        // id 1 is plains -> badlands
-        if (biomeLayer11.sample(0, 0, 0) != 1) {
-            return false;
-        }
-
         /*
         id 38 is wooded_badlands_plateau
         checking at 256:1
@@ -345,20 +307,49 @@ public class OverworldBiomeFilter {
         */
         boolean woodedBadlandsPlateau = false;
         boolean badlandsPlateau = false;
-        for (int x = 0; x < 4; x++) {
-            for (int z = 0; z < 4; z++) {
-                switch (biomeLayer19.sample(x, 0, z)) {
-                    case 38: {
+        for (CPos pos: badlandsPositions) {
+            for (int x = 0; x < 4; x++) {
+                for (int z = 0; z < 4; z++) {
+                    int x_256 = pos.getX() * 4 + x;
+                    int z_256 = pos.getZ() * 4 + z;
+                    if (biomeLayer26.sample(x_256, 0, z_256) == 38) {
                         woodedBadlandsPlateau = true;
-                        break;
-                    }
-                    case 39: {
+                    } else if (biomeLayer26.sample(x_256, 0, z_256) == 39) {
                         badlandsPlateau = true;
-                        break;
                     }
                 }
             }
         }
         return badlandsPlateau && woodedBadlandsPlateau;
+    }
+
+    private boolean hasSnowyBiomes() {
+        /*
+        id 30 is snowy_taiga
+        checking at 256:1
+
+        id 31 is snowy_taiga_hills
+        checking at 64:1
+        */
+        for (CPos pos: snowyPositions) {
+            for (int x = 0; x < 4; x++) {
+                for (int z = 0; z < 4; z++) {
+                    int x_256 = pos.getX() * 4 + x;
+                    int z_256 = pos.getZ() * 4 + z;
+                    if (biomeLayer19.sample(x_256, 0, z_256) == 30) {
+                        for (int i = 0; i < 4; i++) {
+                            for (int j = 0; j < 4; j++) {
+                                int x_64 = x_256 * 4 + i;
+                                int z_64 = z_256 * 4 + j;
+                                if (biomeLayer26.sample(x_64, 0, z_64) == 31) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
